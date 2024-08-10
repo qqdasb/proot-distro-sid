9f21310f5055521744d6a037242e56d6f9a28192
ihaterockchip <126298565+qqdasb@users.noreply.github.com>
2024-08-10 19:37:16 +0800

    Update debian-unstable.sh

--- debian-unstable.sh
+++ debian-unstable.sh
@@ -2,7 +2,7 @@ DISTRO_NAME="Debian (sid)"
 DISTRO_COMMENT="Unstable release."
 
 TARBALL_URL['aarch64']="https://gh.2i.gs/https://github.com/qqdasb/proot-distro-sid/releases/download/v1.0.0/debian-sid-arm64-rootfs.tar.xz"
-TARBALL_SHA256['aarch64']="4d19304566023628591f6e22bd1042a4328e6e5562400417119c9a7a115a2a86"
+TARBALL_SHA256['aarch64']="86485ab70d25f1b0e0340ddec2988ab8e91133cef345d7b638660685ce701bda"
 
 distro_setup() {
 	# Configure zh_CN.UTF-8 locale.
