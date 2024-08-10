7dd6a6c1046f0259b9e0062079b12f285b734692
ihaterockchip <126298565+qqdasb@users.noreply.github.com>
2024-08-10 21:28:41 +0800

    Fix

--- ubuntu-oracular.sh
+++ ubuntu-oracular.sh
@@ -5,7 +5,7 @@ DISTRO_NAME="Ubuntu (24.10)"
 DISTRO_COMMENT="Unstable release (oracular). Not available for x86_64,i386 and armhf CPUs."
 
 TARBALL_URL['aarch64']="https://gh.2i.gs/https://github.com/qqdasb/proot-distro-sid/releases/download/v1.0.0/ubuntu-oracular-arm64-rootfs.tar.xz"
-TARBALL_SHA256['aarch64']="ab2dd580763286f1c9d83f4e1315b608abe6c156bbe5989f13b17b2c005eae1f"
+TARBALL_SHA256['aarch64']="b82c63fad6a5ca17edfd5a4080c61e0ce8650c5e8fb4fcde39d92db0dbf5f54d"
 
 distro_setup() {
 	# Configure zh_CN.UTF-8 locale.
