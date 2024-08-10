6ca5c8bd6f1676b6e158b5c0e2197dfd5d9740d4
ihaterockchip <126298565+qqdasb@users.noreply.github.com>
2024-08-10 21:00:14 +0800

    Fix

--- mmbootstrap-oracular.sh
+++ mmbootstrap-oracular.sh
@@ -2,7 +2,7 @@ sudo mmdebstrap \
 	--mode=sudo \
 	--architectures=arm64 \
 	--variant=standard \
-	--components="main,restricted,universe,multiverse,proposed" \
+	--components="main,restricted,universe,multiverse" \
 	--format=directory \
 	oracular \
 	ubuntu-oracular-arm64
