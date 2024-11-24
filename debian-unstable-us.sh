e02dda98780b8876fd0735f178636c3a8a97d7da
qqdasb <congminda188@outlook.com>
2024-11-24 09:51:27 +0800

    v2.0.0

--- debian-unstable-us.sh
+++ debian-unstable-us.sh
@@ -1,7 +1,7 @@
 DISTRO_NAME="Debian (sid)"
 DISTRO_COMMENT="Unstable release."
 
-TARBALL_URL['aarch64']="https://github.com/qqdasb/proot-distro-sid/releases/download/v1.0.0/debian-sid-arm64-rootfs.tar.xz"
+TARBALL_URL['aarch64']="https://github.com/qqdasb/proot-distro-sid/releases/download/v2.0.0/debian-sid-arm64-rootfs.tar.xz"
 TARBALL_SHA256['aarch64']="337dd150bb1f585df95a28678072a21853b5e732d4e43bfc8960356d7805fa80"
 
 distro_setup() {
