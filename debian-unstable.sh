414f672d59ba84143bfda5bc42f98f323d02870f
qqdasb <congminda188@outlook.com>
2024-11-24 09:46:53 +0800

    Added Ubuntu Oracular and Update Rootfs.

--- debian-unstable.sh
+++ debian-unstable.sh
@@ -2,7 +2,7 @@ DISTRO_NAME="Debian (sid)"
 DISTRO_COMMENT="Unstable release."
 
 TARBALL_URL['aarch64']="https://gh.2i.gs/https://github.com/qqdasb/proot-distro-sid/releases/download/v1.0.0/debian-sid-arm64-rootfs.tar.xz"
-TARBALL_SHA256['aarch64']="86485ab70d25f1b0e0340ddec2988ab8e91133cef345d7b638660685ce701bda"
+TARBALL_SHA256['aarch64']="337dd150bb1f585df95a28678072a21853b5e732d4e43bfc8960356d7805fa80"
 
 distro_setup() {
 	# Configure zh_CN.UTF-8 locale.
--- ubuntu-oracular-us.sh
+++ ubuntu-oracular-us.sh
@@ -4,11 +4,11 @@
 DISTRO_NAME="Ubuntu (24.10)"
 DISTRO_COMMENT="Unstable release (oracular). Not available for x86_64,i386 and armhf CPUs."
 
-TARBALL_URL['aarch64']="https://gh.2i.gs/https://github.com/qqdasb/proot-distro-sid/releases/download/v1.0.0/ubuntu-oracular-arm64-rootfs.tar.xz"
+TARBALL_URL['aarch64']="https://github.com/qqdasb/proot-distro-sid/releases/download/v1.0.0/ubuntu-oracular-arm64-rootfs.tar.xz"
 TARBALL_SHA256['aarch64']="62cc30ab8d4bda3cca4462fa230b738c57694f5c810ba4a86b0a059de657dcc3"
 
 distro_setup() {
-	# Configure zh_CN.UTF-8 locale.
-	sed -i -E 's/#[[:space:]]?(zh_CN.UTF-8[[:space:]]+UTF-8)/\1/g' ./etc/locale.gen
+	# Configure en_US.UTF-8 locale.
+	sed -i -E 's/#[[:space:]]?(en_US.UTF-8[[:space:]]+UTF-8)/\1/g' ./etc/locale.gen
 	run_proot_cmd DEBIAN_FRONTEND=noninteractive dpkg-reconfigure locales
 }
